6c6b240f0feb4756c5106bc24e09e0a35d7aa263
pes4arg <pes4arg@gmail.com>
2023-10-16 10:52:45 -0300

    commit de la guia

--- gitUpload.sh
+++ gitUpload.sh
@@ -2,4 +2,5 @@ cd C:/Users/pes4a/Desktop/UPEPG/EPG1.xml
 git add .
 git commit -m 'commit de la guia'
 git remote add origin https://github.com/pes4arg/EPG1
+git remote add origin https://github.com/pes4arg/play
 git push --force origin master
