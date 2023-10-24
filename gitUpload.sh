dd7a3785cc72abbdf36806f42d7274a91259b4ae
pes4arg <pes4arg@gmail.com>
2023-10-24 07:04:32 -0300

    commit de la guia

--- gitUpload.sh
+++ gitUpload.sh
@@ -1,7 +1,6 @@
 cd C:/Users/pes4a/Desktop/UPEPG/EPG1.xml
 git add .
 git commit -m 'commit de la guia'
-git remote add origin https://github.com/pes4arg/EPG1
 git remote add origin https://github.com/pes4arg/play
-git remote add origin https://github.com/pes4arg/guide
+git remote add origin https://github.com/pes4arg/total
 git push --force origin master
--- move.bat
+++ move.bat
@@ -1,1 +1,1 @@
-MOVE "C:\Users\pes4a\Desktop\webplus\epge\ConfigMaker_0.2.4.0\output\epg1.xml" "C:\Users\pes4a\Desktop\UPEPG"
+MOVE "C:\Users\pes4a\Desktop\webplus\epge\ConfigMaker_0.2.4.0\output\total.xml" "C:\Users\pes4a\Desktop\UPEPG"
